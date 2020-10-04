33a00a94e676c7c934fccb9475ab5318d7c1c14a
douglasmatosdev <douglasmatosdev@gmail.com>
2020-10-04 10:05:16 -0300

    Draw effect with CSS and SVG

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import LogoTwitter from './LogoTwitter';
+import Assignature from './Assignature'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const svgTwitterRef = React.createRef<SVGSVGElement>()
+    const svgAssignatureRef = React.createRef<SVGSVGElement>()
+
+
+    React.useEffect(() => {
+        if (svgAssignatureRef.current) {
+
+            const pathAssignatureSVG = svgAssignatureRef.current.querySelector('path');
+            const assignaturePathLength = pathAssignatureSVG?.getTotalLength();
+            svgAssignatureRef.current.style.setProperty('--assignaturePathLength', String(assignaturePathLength));
+
+        }
+        if (svgTwitterRef.current) {
+            const pathTwitterSVG = svgTwitterRef.current.querySelector('path');
+            const twitterPathLength = pathTwitterSVG?.getTotalLength();
+            svgTwitterRef.current.style.setProperty('--twitterPathLength', String(twitterPathLength));
+        }
+    }, [svgAssignatureRef.current, svgTwitterRef.current])
+
+    return (
+        <div className="App">
+            <div className="Assignature">
+                <Assignature ref={svgAssignatureRef} />
+            </div>
+            <div className="Twitter">
+                <LogoTwitter ref={svgTwitterRef} />
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default App
